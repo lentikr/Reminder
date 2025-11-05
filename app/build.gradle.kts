d232827088cc6fb9c935c6b411727a5424a65436
lentikr <xujingao@nudt.edu.cn>
2025-11-05 09:18:02 +0800

    chore: Bump version number
    This commit increments the app version for the next release.
    
    - The `versionCode` is updated from `10` to `11`.
    - The `versionName` is updated from "1.6.2" to "1.6.3".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.lentikr.reminder"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.6.2"
+        versionCode = 11
+        versionName = "1.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
